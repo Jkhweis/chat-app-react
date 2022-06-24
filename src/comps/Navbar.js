5b3adcfb13de519814688a206b0485ef7ecfd9b5
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-06-24 09:12:20 -0500

    Create Logout Function

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,3 +1,25 @@
+import { Link } from 'react-router-dom';
+import { UserAuth } from '../context/AuthContext';
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const { user, logOut } = UserAuth();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Navbar</h1>
+      {user ? (
+        <button onClick={handleLogout}>Logout</button>
+      ) : (
+        <Link to="/signin">Sign in</Link>
+      )}
+    </div>
+  );
 }
