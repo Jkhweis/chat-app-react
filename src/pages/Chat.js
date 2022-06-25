e4616925e723f2cc704055acf0612746799fa92d
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-06-25 16:38:50 -0500

    Add Add Document case to useFirestore Reducer Function

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,7 @@
 export default function Chat() {
   return (
     <div>
-      <p>testing...Hi! this is the amazing chat page</p>
+      <p>testing...Hi! this is the amazing chat page!</p>
     </div>
   );
 }
